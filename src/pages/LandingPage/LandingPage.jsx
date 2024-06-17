e47406b4c8f9eba47df3f0b4e026cb7c2a6de40d
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-06-17 13:17:23 +0530

    Added the landing Page

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./LandingPage.css";
+
+const LandigPage = ({}) => {
+React.useEffect(() => {
+// Initialize the code
+return () => {}
+}, [])
+return (
+<div className={'landing-page'}>
+    <div className={'frame-1'}>
+        <img src="https://image-resource.creatie.ai/127027295676199/127027295676201/fee8dcc68324dddddec89d2635056fe1.png" className={'unsplashwtbjpqu-0vq'} />
+        <div className={'rectangle-1'}>
+        </div>
+    </div>
+    <div className={'frame-2'}>
+        <div className={'rectangle-2'}>
+        </div>
+        <img src="https://image-resource.creatie.ai/127027295676199/127027295676201/433b812c0a9d44e397ebe3c48ba9107f.png" className={'asset-2-1'} />
+    </div>
+    <div className={'saarthi'}>
+        SAARTHI
+    </div>
+    <div className={'one-stop-for-all-tours'}>
+        One Stop for all Tours
+    </div>
+    <div className={'frame-3'}>
+        <div className={'get-started'}>
+            Get Started
+        </div>
+        <svg id="0:33" className={'icon-iconoir-arrow-right'}></svg>
+    </div>
+</div>
+);
+};
+export default LandigPage;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 
 // Pages Import 
-import HeroSection from '../components/HeroSection';
+
 import DiscoverIndia from '../pages/DiscoverIndia'; 
 import BookSlot from '../pages/BookSlot'
 import AboutUs from '../pages/AboutUs'
 import ContactUs from '../pages/ContactUs'
+import LandigPage from '../pages/LandingPage/LandingPage';
 
 const MainRoutes = () => {
   return (
     <Routes> 
-        <Route path="/" element={<HeroSection />} /> 
+        <Route path="/" element={<LandigPage />} /> 
         <Route path="/discover-india" element={<DiscoverIndia />} /> 
         <Route path="/book-slot" element={< BookSlot/>} /> 
         <Route path="/about" element={< AboutUs/>} /> 
